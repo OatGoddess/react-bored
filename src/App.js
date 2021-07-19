8448f27c2ab8ffd52d3d154f9b9cb428a3d20a58
RayneTempest <rayne.lovelace@gmail.com>
2021-07-19 11:57:49 -0700

    adding endpoint and example return

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import './App.css'
 
+/*
+endpoint: https://www.boredapi.com/api/activity
+
+example:
+{
+  accessibility: 0.3
+  activity: "Learn origami"
+  key: "8394738"
+  link: ""
+  participants: 1
+  price: 0.2
+  type: "education"
+}
+*/
 function App() {
   return <div className="App"></div>
 }
